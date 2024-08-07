73fb37061836431abd67191dcd93969944809c4a
RomainP <romainportier7@gmail.com>
2024-08-07 17:34:20 +0200

    add basic theme list display

--- front/src/app/features/themes/components/list/list.component.ts
+++ front/src/app/features/themes/components/list/list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Theme } from '../../interfaces/theme';
+import { ThemeApiService } from '../../services/theme-api.service';
 
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+
+  public themes$: Observable<Theme[]> = this.themeApiService.all();
+
+  constructor(private themeApiService: ThemeApiService){
+
+  }
 
 }
--- front/src/app/features/themes/themes.module.ts
+++ front/src/app/features/themes/themes.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ThemesRoutingModule } from './themes-routing.module';
+import { ListComponent } from './components/list/list.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
+const materialModules = [
+  MatGridListModule,
+  MatCardModule,
+  MatButtonModule,
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent],
   imports: [
     CommonModule,
-    ThemesRoutingModule
+    ThemesRoutingModule,
+    ...materialModules
   ]
 })
 export class ThemesModule { }
