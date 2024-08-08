ce57e167cba7a683a58d585c88d60f0a33fa309b
RomainP <romainportier7@gmail.com>
2024-08-08 18:05:02 +0200

    add basic login component

--- front/src/app/features/auth/component/login/login.component.ts
+++ front/src/app/features/auth/component/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public form = this.builder.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+      ]
+    ]
+  });
 
+  constructor(private builder: FormBuilder){}
+
+  public submit(): void {
+    
+  }
 }
