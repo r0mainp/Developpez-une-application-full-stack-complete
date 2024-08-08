785613e675d1c4241bfca81b01f7d726144d7067
RomainP <romainportier7@gmail.com>
2024-08-08 17:30:43 +0200

    add Login and register button on homepage + login redirection

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   {
     path: '',
     loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
