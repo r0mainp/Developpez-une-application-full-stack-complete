59562c806cd1db14ff7871946ee1d8caf21beda0
RomainP <romainportier7@gmail.com>
2024-08-10 15:23:31 +0200

    add missing GetMapping

--- back/src/main/java/com/openclassrooms/mddapi/controllers/AuthController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +63,7 @@ public class AuthController {
         }
     }
 
+    @GetMapping("/me")
     public ResponseEntity<?> authenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
