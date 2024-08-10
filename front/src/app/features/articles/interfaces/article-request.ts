4e835f4e64e39dc190144ba0f4b6e3160c2da1d5
RomainP <romainportier7@gmail.com>
2024-08-11 01:58:38 +0200

    add articles interfaces

--- front/src/app/features/articles/interfaces/article-request.ts
+++ front/src/app/features/articles/interfaces/article-request.ts
@@ -1,2 +1,5 @@
 export interface ArticleRequest {
+    title: string;
+    content: string;
+    theme: number;
 }
--- front/src/app/features/articles/interfaces/article.ts
+++ front/src/app/features/articles/interfaces/article.ts
@@ -1,2 +1,10 @@
 export interface Article {
+    id?: number;
+    title: string;
+    content: string;
+    author: number;
+    theme: number;
+    created_at?: string;
+    updated_at?: string;
+
 }
