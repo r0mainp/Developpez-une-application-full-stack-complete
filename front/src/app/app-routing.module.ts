d9018694f594c8f1e1555810fe9171b206b33562
RomainP <romainportier7@gmail.com>
2024-08-10 15:25:35 +0200

    add guards to route

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UnauthGuard } from './core/guards/unauth.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { 
+    canActivate: [UnauthGuard],
+    path: '', 
+    component: HomeComponent 
+  },
   {
+    canActivate: [UnauthGuard],
     path: '',
     loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
   },
   {
+    canActivate: [AuthGuard],
     path: 'themes',
     loadChildren: () => import('./features/themes/themes.module').then(m => m.ThemesModule)
   }
