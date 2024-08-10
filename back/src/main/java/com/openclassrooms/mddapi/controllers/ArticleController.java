af3f1e9e9ac7124f12b2128a4675cbf83da4b8ea
RomainP <romainportier7@gmail.com>
2024-08-11 01:00:38 +0200

    add Article controller with /, /{id} and /create endpoints

---
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
@@ -0,0 +1,71 @@
+package com.openclassrooms.mddapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.mddapi.models.Article;
+import com.openclassrooms.mddapi.models.Theme;
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.payload.request.ArticleRequest;
+import com.openclassrooms.mddapi.services.ArticleService;
+import com.openclassrooms.mddapi.services.ThemeService;
+import com.openclassrooms.mddapi.services.UserService;
+
+@RestController
+@RequestMapping("/api/article")
+public class ArticleController {
+
+    @Autowired
+    ThemeService themeService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ArticleService articleService;
+
+    private User currentUser = this.userService.getCurrentUser();
+
+
+    @GetMapping()
+    public ResponseEntity<?> findAllArticleFromSubscribedThemes() {
+        if (currentUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
+        }
+
+        List<Article> articles = this.articleService.findAllArticlesByUserSubscriptions(currentUser.getId());
+        return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable("id") String id){
+        try {
+            Article theme = this.articleService.findById(Integer.parseInt(id));
+    
+            return ResponseEntity.ok().body(theme);
+        }catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public Article createArticle(ArticleRequest request){
+        Theme relatedTheme = this.themeService.findById(request.getThemeId());
+
+        Article articleToCreate = new Article()
+            .setAuthor(currentUser)
+            .setTheme(relatedTheme)
+            .setTitle(request.getTitle())
+            .setContent(request.getContent());
+
+        return this.articleService.create(articleToCreate);
+    }
+}
--- back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
@@ -3,6 +3,9 @@ package com.openclassrooms.mddapi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.mddapi.models.User;
@@ -16,5 +19,18 @@ public class UserService {
 
     public Optional<User> getUserById(final Integer id) {
         return userRepository.findById(id);
+    }
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            
+            if (principal instanceof UserDetails) {
+                String username = ((UserDetails) principal).getUsername();
+                return userRepository.findByEmail(username).orElse(null);
+            }
+        }
+        return null;
     }
 }
