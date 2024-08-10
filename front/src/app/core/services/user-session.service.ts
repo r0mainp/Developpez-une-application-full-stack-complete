849fecf7e48f4a13962c870cc457d42d68e8839d
RomainP <romainportier7@gmail.com>
2024-08-10 18:19:25 +0200

    log or logout user if token is valid/invalid on app loading

--- front/src/app/core/services/user-session.service.ts
+++ front/src/app/core/services/user-session.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, of, switchMap, throwError } from 'rxjs';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,33 @@ export class UserSessionService {
   public user: User | undefined;
 
   private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged)
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.me().pipe(
+        switchMap((user: User) => {
+          if (user) {
+            this.logIn(user);
+            this.router.navigate(['/themes']);
+          }
+          return of(null);
+        }),
+        catchError((error) => {
+          if (error.status === 403) {
+            this.logOut();
+            return of(null);
+          }
+          return throwError(() =>new Error(error));
+        }),
+      ).subscribe();
+    } else {
+      this.isLoggedSubject.next(false);
+    }
+  }
 
   public $isLogged(): Observable<boolean>{
     return this.isLoggedSubject.asObservable();
