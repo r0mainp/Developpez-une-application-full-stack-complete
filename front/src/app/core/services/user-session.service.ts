07e922db5f843f50b276c5d7973865dbae5af948
RomainP <romainportier7@gmail.com>
2024-08-10 18:23:43 +0200

    make sure to unsubscribe on component destroy

--- front/src/app/core/services/user-session.service.ts
+++ front/src/app/core/services/user-session.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { User } from '../interfaces/user';
-import { BehaviorSubject, catchError, Observable, of, switchMap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, of, Subscription, switchMap, throwError } from 'rxjs';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserSessionService {
+export class UserSessionService implements OnDestroy {
   public isLogged = false;
   public user: User | undefined;
 
   private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged)
+  private subscription: Subscription = new Subscription();
 
   constructor(
     private authService: AuthService,
@@ -19,25 +20,31 @@ export class UserSessionService {
   ){
     const token = localStorage.getItem('token');
     if (token) {
-      this.authService.me().pipe(
-        switchMap((user: User) => {
-          if (user) {
-            this.logIn(user);
-            this.router.navigate(['/themes']);
-          }
-          return of(null);
-        }),
-        catchError((error) => {
-          if (error.status === 403) {
-            this.logOut();
+      this.subscription.add(
+        this.authService.me().pipe(
+          switchMap((user: User) => {
+            if (user) {
+              this.logIn(user);
+              this.router.navigate(['/themes']);
+            }
             return of(null);
-          }
-          return throwError(() =>new Error(error));
-        }),
-      ).subscribe();
+          }),
+          catchError((error) => {
+            if (error.status === 403) {
+              this.logOut();
+              return of(null);
+            }
+            return throwError(() =>new Error(error));
+          }),
+        ).subscribe()
+      );
     } else {
       this.isLoggedSubject.next(false);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   public $isLogged(): Observable<boolean>{
