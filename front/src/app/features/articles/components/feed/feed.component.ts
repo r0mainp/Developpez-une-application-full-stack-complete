2900cad7e16c66f686f22790b85970105571e0da
RomainP <romainportier7@gmail.com>
2024-08-11 19:40:33 +0200

    add redirection to form

--- front/src/app/features/articles/components/feed/feed.component.ts
+++ front/src/app/features/articles/components/feed/feed.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
 import { Article } from '../../interfaces/article';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feed',
@@ -14,10 +15,15 @@ export class FeedComponent {
 
   constructor(
     private articleService: ArticleService,
+    private router: Router
   ){}
 
   public updateOrder():void {
     this.sortOrder = this.sortOrder == 'desc'?'asc': 'desc';
     this.articles$ = this.articleService.all(this.sortOrder)
+  }
+
+  goToForm(){
+    this.router.navigate(["/create"])
   }
 }
