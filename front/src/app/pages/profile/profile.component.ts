e62d8b3cad7f600de22ba2370b9b48d9ce99dd7e
RomainP <romainportier7@gmail.com>
2024-08-13 00:07:01 +0200

    add profile page with functionning form and style

--- front/src/app/pages/profile/profile.component.ts
+++ front/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Observable, tap } from 'rxjs';
+import { User } from 'src/app/core/interfaces/user';
+import { UserSessionService } from 'src/app/core/services/user-session.service';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  public user$!: Observable<User>;
 
+  public form = this.builder.group({
+    username: [
+      '',
+      [
+        Validators.required,
+      ]
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+      ]
+    ],
+  });
+
+  constructor(
+    private builder: FormBuilder,
+    private authService: AuthService,
+    private userSessionService: UserSessionService
+  ){}
+
+  ngOnInit() {
+    this.user$ = this.authService.me().pipe(
+      tap(user => this.form.patchValue(
+        {
+          username: user.username,
+          email: user.email
+        }
+      ))
+    );
+  }
+
+  public submit():void{
+    console.log(this.form.value)
+  }
+  public logout(): void{
+    this.userSessionService.logOut();
+  }
 }
