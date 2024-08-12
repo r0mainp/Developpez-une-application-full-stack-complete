a651d9fac737c0d056165f33f48e766c20ff77a4
RomainP <romainportier7@gmail.com>
2024-08-12 08:34:11 +0200

    display username and comment content in comment component using service

--- front/src/app/features/articles/components/comment/comment.component.ts
+++ front/src/app/features/articles/components/comment/comment.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommentService } from '../../services/comment.service';
+import { Observable } from 'rxjs';
+import { Comment } from '../../interfaces/comment';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
-export class CommentComponent {
+export class CommentComponent implements OnInit{
+    @Input() public articleId!: number;
+    public comments$!: Observable<Comment[]>
+    constructor(
+      private commentService: CommentService
+    ){}
 
+    ngOnInit(): void {
+        console.log()
+        this.comments$ = this.commentService.all(this.articleId);
+    }
 }
--- front/src/app/features/articles/services/comment.service.ts
+++ front/src/app/features/articles/services/comment.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Comment } from '../interfaces/comment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class CommentService {
   constructor(private httpClient: HttpClient) { }
 
   public all(articleId: number, sortOrder: string = 'desc'): Observable<Comment[]> {
+    console.log(articleId)
     return this.httpClient.get<Comment[]>(`${this.pathService}?articleId=${articleId}&sort=${sortOrder}`);
   }
 }
