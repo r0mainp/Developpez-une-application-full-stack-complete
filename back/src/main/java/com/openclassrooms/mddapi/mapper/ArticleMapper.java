2b1310906520f77984e03531d55804067a52c4d2
RomainP <romainportier7@gmail.com>
2024-08-13 05:52:17 +0200

    add javadoc for mappers

--- back/src/main/java/com/openclassrooms/mddapi/mapper/ArticleMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/ArticleMapper.java
@@ -7,11 +7,34 @@ import org.mapstruct.factory.Mappers;
 import com.openclassrooms.mddapi.dto.ArticleDto;
 import com.openclassrooms.mddapi.models.Article;
 
+/**
+ * Mapper interface for converting between {@link Article} entities and {@link ArticleDto} data transfer objects.
+ * Uses MapStruct to generate the implementation for mapping fields between the two types.
+ */
 @Mapper(componentModel = "spring")
 public interface ArticleMapper {
 
+    /**
+     * The singleton instance of {@link ArticleMapper}.
+     * <p>
+     * This instance is used for mapping operations within the application.
+     * </p>
+     */
     ArticleMapper INSTANCE = Mappers.getMapper(ArticleMapper.class);
 
+    /**
+     * Converts an {@link Article} entity to an {@link ArticleDto} data transfer object.
+     * <p>
+     * The mapping configuration includes:
+     * <ul>
+     *     <li>Mapping the "theme.id" field of {@link Article} to the "themeId" field of {@link ArticleDto}.</li>
+     *     <li>Mapping the "author.id" field of {@link Article} to the "authorId" field of {@link ArticleDto}.</li>
+     * </ul>
+     * </p>
+     * 
+     * @param article The {@link Article} entity to be converted.
+     * @return The corresponding {@link ArticleDto} data transfer object.
+     */
     @Mapping(source = "theme.id", target = "themeId")
     @Mapping(source = "author.id", target = "authorId")
     ArticleDto toDto(Article article);
