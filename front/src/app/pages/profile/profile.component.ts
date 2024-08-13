896ff9c26015ce2657a11e031c2ee0bbe2304655
RomainP <romainportier7@gmail.com>
2024-08-13 06:53:44 +0200

    add TsDoc for pages files

--- front/src/app/pages/profile/profile.component.ts
+++ front/src/app/pages/profile/profile.component.ts
@@ -12,15 +12,37 @@ import { AuthService } from 'src/app/features/auth/services/auth.service';
 import { SubscriptionService } from 'src/app/core/services/subscription.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * The `ProfileComponent` is responsible for managing and displaying the user's profile information.
+ * It allows users to update their profile details and view their current subscriptions along with associated themes.
+ * The component interacts with various services to retrieve user data, subscriptions, and themes.
+ *
+ * @component
+ * @implements OnInit
+ * @selector app-profile
+ * @templateUrl ./profile.component.html
+ * @styleUrl ./profile.component.scss
+ */
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
+  /**
+   * An observable of the current user, used to populate the form with user data.
+   */
   public user$!: Observable<User>;
+
+  /**
+   * An observable of subscriptions combined with their associated themes.
+   * Each subscription object includes the associated theme details.
+   */
   public subscriptionsWithThemes$!: Observable<(Subscription & { theme?: Theme })[]>;
 
+  /**
+   * Reactive form for handling user profile data such as username, email, and id.
+   */
   public form = this.builder.group({
     username: [
       '',
@@ -42,6 +64,17 @@ export class ProfileComponent implements OnInit{
     ],
   });
 
+  /**
+   * Constructor for `ProfileComponent`.
+   * 
+   * @param builder - FormBuilder service to create and manage the reactive form.
+   * @param authService - AuthService to retrieve the current user details.
+   * @param userSessionService - UserSessionService to manage user sessions, including logout.
+   * @param userService - UserService to update the user profile data.
+   * @param themeService - ThemeService to retrieve available themes.
+   * @param subscriptionService - SubscriptionService to manage user subscriptions.
+   * @param matSnackBar - MatSnackBar to display notifications to the user.
+   */
   constructor(
     private builder: FormBuilder,
     private authService: AuthService,
@@ -50,8 +83,12 @@ export class ProfileComponent implements OnInit{
     private themeService: ThemeService,
     private subscriptionService: SubscriptionService,
     private matSnackBar: MatSnackBar,
-  ){}
+  ) {}
 
+  /**
+   * Initializes the component by fetching the current user's data
+   * and loading the user's subscriptions along with their associated themes.
+   */
   ngOnInit() {
     this.user$ = this.authService.me().pipe(
       tap(user => this.form.patchValue(
@@ -63,12 +100,13 @@ export class ProfileComponent implements OnInit{
       ))
     );
 
-    const subscriptions$ = this.subscriptionService.all();
-    const themes$ = this.themeService.all();
-
     this.loadSubscriptionsWithThemes();
   }
 
+  /**
+   * Loads the subscriptions and their associated themes and combines them into
+   * a single observable (`subscriptionsWithThemes$`).
+   */
   private loadSubscriptionsWithThemes() {
     const subscriptions$ = this.subscriptionService.all();
     const themes$ = this.themeService.all();
@@ -86,7 +124,11 @@ export class ProfileComponent implements OnInit{
     );
   }
 
-  public submit():void{
+  /**
+   * Submits the form data to update the user's profile information.
+   * Displays a snackbar notification with the response message.
+   */
+  public submit(): void {
     const username = this.form.value.username as string;
     const email = this.form.value.email as string;
     const id = this.form.value.id as number;
@@ -95,16 +137,23 @@ export class ProfileComponent implements OnInit{
       username: username,
       email: email,
       id: id,
-    }
+    };
 
-    this.userService.update(request).subscribe((response)=>{
+    this.userService.update(request).subscribe((response) => {
       this.matSnackBar.open(response.message, "Close", { duration: 3000 });
-    })
+    });
   }
-  public logout(): void{
+
+  /**
+   * Logs the user out by calling the `logOut` method of `UserSessionService`.
+   */
+  public logout(): void {
     this.userSessionService.logOut();
   }
 
+  /**
+   * Refreshes the subscriptions and their associated themes by reloading the data.
+   */
   public handleRefresh() {
     this.loadSubscriptionsWithThemes();
   }
